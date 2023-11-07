dbaa8f566aefcadd02bd8ca32074eee366862b8a
JoshHumpherey <jhumphrey321@gmail.com>
2023-11-06 18:29:04 -0600

    Finish 2018 Day 14 Part 2

--- 2018/day14/solution.py
+++ 2018/day14/solution.py
@@ -1,3 +1,4 @@
+import os
 from lib.parse import parse_strings
 
 def get_next_idx(recipes: str, idx: int):
@@ -22,5 +23,30 @@ def get_best_recipes(size: int) -> str:
         elf_2 = get_next_idx(recipes, elf_2)
     
     return recipes[size:size+buffer]
+
+def matches_pattern(recipes: str, pattern: str) -> bool:
+    if len(pattern) > len(recipes):
+        return False
+    elif recipes == pattern:
+        return True
     
+    n = len(recipes)
+    chunk = recipes[n-len(pattern):]
+    return chunk == pattern
+
+def get_recipes_before_pattern(pattern: str) -> int:
+    recipes = parse_strings("2018/day14/input.txt")[0]
+    elf_1, elf_2 = 0, 1
+    while True:
+        new_recipe = int(recipes[elf_1]) + int(recipes[elf_2])
+        recipes += str(new_recipe)
+        elf_1 = get_next_idx(recipes, elf_1)
+        elf_2 = get_next_idx(recipes, elf_2)
+        if matches_pattern(recipes=recipes, pattern=pattern):
+            return len(recipes) - len(pattern)
+        elif len(recipes) % 10_000 == 0:
+            os.system("clear")
+            print(f"Evaluated {len(recipes)}")
+
 print(get_best_recipes(size=360781))
+print(get_recipes_before_pattern(pattern="360781"))
