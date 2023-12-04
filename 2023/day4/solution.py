8a2c3fd0e0723417279c57860d8115256fb966aa
JoshHumpherey <jhumphrey321@gmail.com>
2023-12-04 10:07:51 -0600

    Finish 2023 Day 4 Part 2

--- 2023/day4/solution.py
+++ 2023/day4/solution.py
@@ -1,4 +1,6 @@
+from typing import List
 from lib.parse import parse_strings
+from collections import defaultdict
 
 class Scratchoff:
 
@@ -20,6 +22,18 @@ class Scratchoff:
         elif hits == 1:
             return 1
         return 2 ** (hits-1)
+    
+    def winning_card_numbers(self) -> List[int]:
+        hits = 0
+        for n in self.card_numbers:
+            if n in self.winning_numbers:
+                hits += 1
+        
+        winners = []
+        for i in range(self.id+1, self.id+1+hits):
+            winners.append(i)
+
+        return winners
         
 
 def score_cards() -> None:
@@ -31,4 +45,33 @@ def score_cards() -> None:
     
     return res
 
+def total_winnings() -> None:
+    data = parse_strings("2023/day4/input.txt")
+    queue: List[Scratchoff] = []
+    mapping = {}
+
+    for d in data:
+        s = Scratchoff(raw_input=d)
+        queue.append((s, 1))
+        mapping[s.id] = s
+
+    total = 0
+    while queue:
+        next_counts = defaultdict(int)
+        for card, amt in queue:
+            total += amt
+            winning_numbers = card.winning_card_numbers()
+            for next_card_id in winning_numbers:
+                next_counts[next_card_id] += amt
+
+        next_queue = []
+        for key, val in next_counts.items():
+            if val > 0:
+                next_queue.append([mapping[key], val])
+        queue = next_queue
+
+    return total
+    
+
 print(score_cards())
+print(total_winnings())
