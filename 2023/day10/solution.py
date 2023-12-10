26e1b1c5358d1625373ca19fe944cab02aab9f5a
JoshHumpherey <jhumphrey321@gmail.com>
2023-12-10 14:58:44 -0600

    Finish 2023 Day 10 Part 2

--- 2023/day10/solution.py
+++ 2023/day10/solution.py
@@ -57,8 +57,40 @@ def pipe_bfs(grid: List[List[str]], start_row: int, start_col: int) -> int:
 
     return dist-1 if dist > 0 else 0
 
+def get_boundary_points(grid: List[List[str]], start_row: int, start_col: int) -> set:
+    boundary = set()
+    queue = [[start_row, start_col]]
 
-def farthest_pipe_dist() -> None:
+    while queue:
+        next_queue = []
+        for r,c in queue:
+            boundary.add((r,c))
+            connections = get_connections(grid, r, c)
+            for rr, cc in connections:
+                if is_valid(grid, rr, cc) and (rr, cc) not in boundary:
+                    next_queue.append([rr, cc])
+        
+        queue = next_queue
+    
+    return boundary
+
+def print_boundary(grid: List[List[str]], boundary: set, enclosed: set) -> None:
+    for r in range(len(grid)):
+        row = ""
+        for c in range(len(grid[0])):
+            if (r,c) in boundary:
+                if grid[r][c] in {"|", "L", "J"}:
+                    row += "|"
+                else:
+                    row += "-"
+            elif (r,c) in enclosed:
+                row += "@"
+            else:
+                row += grid[r][c]
+        print(row)
+
+
+def farthest_pipe_dist() -> int:
     grid = parse_string_grid("2023/day10/input.txt")
     for r in range(len(grid)):
         for c in range(len(grid[0])):
@@ -66,5 +98,31 @@ def farthest_pipe_dist() -> None:
                 return pipe_bfs(grid, r, c)
     
     return -1
+
+def get_enclosed_area() -> int:
+    grid = parse_string_grid("2023/day10/input.txt")
+    boundary = set()
+
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            if grid[r][c] == "S":
+                boundary = get_boundary_points(grid, r, c)
+                break
     
+    enclosed = set()
+    for r in range(len(grid)):
+        inside = False
+        for c in range(len(grid[0])):
+            if (r,c) in boundary and grid[r][c] in {"|", "L", "J"}:
+                if inside:
+                    inside = False
+                else:
+                    inside = True
+            elif (r,c) not in boundary and inside:
+                enclosed.add((r,c))
+
+    print_boundary(grid=grid, boundary=boundary, enclosed=enclosed)
+    return len(enclosed)
+
 print(farthest_pipe_dist())
+print(get_enclosed_area())
