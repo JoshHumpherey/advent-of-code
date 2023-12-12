d6af9670171115d177b1675d6d66f602c8361ce1
JoshHumpherey <jhumphrey321@gmail.com>
2023-12-12 16:43:17 -0600

    Optimize 2023 Day 12 using memoization

--- 2023/day12/solution.py
+++ 2023/day12/solution.py
@@ -1,5 +1,6 @@
+import functools
 import os
-from typing import List
+from typing import List, Tuple
 from lib.parse import parse_strings
 
 def get_springs():
@@ -12,17 +13,38 @@ def get_springs():
         springs.append([diagram, nums])
     return springs
 
+def get_expanded_springs():
+    data = parse_strings("2023/day12/input.txt")
+    springs = []
+    for d in data:
+        diagram, nums = d.split(" ")
+        diagram = list(diagram)
+        nums = [int(x) for x in nums.split(",")]
+
+        expanded_diagram = []
+        expanded_nums = []
+        for i in range(0, 5):
+            expanded_diagram.extend(diagram)
+            expanded_nums.extend(nums)
+            if i < 4:
+                expanded_diagram.extend("?")
+        springs.append([expanded_diagram, expanded_nums])
+
+    return springs
+
+
 def get_possibilities(diagram: List[str], nums: List[int]) -> int:
     potential = []
 
-    def generate(idx: int, l: List[str]) -> None:
+    @functools.lru_cache
+    def generate(idx: int, l: Tuple) -> None:
         if idx >= len(diagram):
             potential.append(l)
         elif diagram[idx] == "?":
-            generate(idx+1, l + ["."])
-            generate(idx+1, l + ["#"])
+            generate(idx+1, l + (".",))
+            generate(idx+1, l + ("#",))
         else:
-            generate(idx+1, l + [diagram[idx]])
+            generate(idx+1, l + (diagram[idx],))
     
     def score(l: List[str]) -> List[int]:
         curr = ""
@@ -40,42 +62,62 @@ def get_possibilities(diagram: List[str], nums: List[int]) -> int:
         
         return res
 
-    generate(0, [])
+    generate(0, ())
     total = 0
     for p in potential:
         if score(p) == nums:
             total += 1
     return total
 
+def get_possibilities_memo(diagram: List[str], nums: List[int]) -> int:
+    # print(f"{''.join(diagram)}, {nums}")
+
+    @functools.lru_cache
+    def generate_efficient(idx: int, streak: int, nums: Tuple) -> int:
+        if idx >= len(diagram):
+            if (not nums and streak == 0) or (len(nums) == 1 and nums[0] == streak):
+                return 1
+
+            return 0
+        elif nums and streak > nums[0]:
+            return 0
+        
+        res = 0
+        if diagram[idx] in {"?", "#"}:
+            res += generate_efficient(idx+1, streak+1, nums)
+        if diagram[idx] in {"?","."}:
+            if nums and streak == nums[0]:
+                res += generate_efficient(idx+1, 0, nums[1:])                
+            elif streak == 0:
+                res += generate_efficient(idx+1, 0, nums)
+
+        return res
+    
+    tuple_nums = ()
+    for n in nums:
+        tuple_nums += (n,)
+
+    return generate_efficient(idx=0, streak=0, nums=tuple_nums)
+
+
 def get_sums() -> int:
     springs = get_springs()
     res = 0
-
+    
     for diagram, nums in springs:
-        local = get_possibilities(diagram, nums)
-        res += local
+        res += get_possibilities_memo(diagram, nums)
 
     return res
 
 def get_expanded_sums() -> int:
-    springs = get_springs()
-    for i in range(len(springs)):
-        orig_diagram = springs[i][0]
-        orig_nums = springs[i][1]
-        diagram, nums = [], []
-        for _ in range(5):
-            diagram.extend(orig_diagram)
-            nums.extend(orig_nums)
-        springs[i] = [diagram, nums]   
-
+    springs = get_expanded_springs()
     res = 0
-    count = 0
+
     for diagram, nums in springs:
-        os.system("clear")
-        print(f"Processing {count}")
-        res += get_possibilities(diagram, nums)
-        count += 1
+        local = get_possibilities_memo(diagram, nums)
+        res += local
     
     return res
 
+print(get_sums())
 print(get_expanded_sums())
