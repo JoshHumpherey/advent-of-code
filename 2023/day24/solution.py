bfd24df42d261cf01106949e25a4ed9d377e3efe
JoshHumpherey <jhumphrey321@gmail.com>
2023-12-24 16:49:45 -0500

    Finish 2023 Day 24 Part 1

--- 2023/day24/solution.py
+++ 2023/day24/solution.py
@@ -1,6 +1,6 @@
 from typing import List
 from lib.parse import parse_strings
-import z3
+import numpy as np
 
 class Hailstone:
 
@@ -11,28 +11,9 @@ class Hailstone:
         self.vx = vel[0]
         self.vy = vel[1]
         self.vz = vel[2]
-        
-def count_collisions(hailstones: List[Hailstone], low_bound: int, high_bound: int) -> int:
-    x, y, z = z3.Real('x'), z3.Real('y'), z3.Real('z')
-    vx, vy, vz = z3.Real('vx'), z3.Real('vy'), z3.Real('vz')
-    s = z3.Solver()
-    for i in range(len(hailstones)):
-        x_i, y_i, z_i = hailstones[i].x, hailstones[i].y, hailstones[i].z
-        vx_i, vy_i, vz_i = hailstones[i].vx, hailstones[i].vy, hailstones[i].vz
-        t_i = z3.Real(f"t_{i}")
-        s.add(x_i + vx_i * t_i == x + vx * t_i)
-        s.add(y_i + vy_i * t_i == y + vy * t_i)
-
-    s.add(low_bound <= x, x <= high_bound)
-    s.add(low_bound <= y, y <= high_bound)
-
-    count = 0
-    while s.check() == z3.sat:
-        count += 1
-        m = s.model()
-        s.add(z3.Or(x != m[x], y != m[y]))
-
-    return count
+    
+    def info(self) -> str:
+        return f"Pos: {self.x, self.y, self.z}, Vel: {self.vx, self.vy, self.vz}"
 
 def create_hailstones() -> List[Hailstone]:
     data = parse_strings("2023/day24/input.txt")
@@ -40,13 +21,27 @@ def create_hailstones() -> List[Hailstone]:
     for d in data:
         raw = d.split(" @ ")
         pos, vel = [[int(x) for x in y.split(", ")] for y in raw]
-        print(pos, vel)
         hailstones.append(Hailstone(pos=pos, vel=vel))
     
     return hailstones
 
+def collisions_within_bounds(hailstones: List[Hailstone], low_bound: int, high_bound: int):
+    collisions = 0
+    for i in range(len(hailstones)):
+        for j in range(i + 1, len(hailstones)):
+            h1, h2 = hailstones[i], hailstones[j]
+            if h2.vy == 0 or h1.vy == 0 or (h1.vx / h1.vy - h2.vx / h2.vy) == 0:
+                continue
+            y = (h2.x - (h2.vx / h2.vy * h2.y) + (h1.vx / h1.vy * h1.y) - h1.x) / (h1.vx / h1.vy - h2.vx / h2.vy)
+            x = ((y - h1.y) / h1.vy) * h1.vx + h1.x
+            if (low_bound <= x <= high_bound and low_bound <= y <= high_bound):
+                if (np.sign(x - h1.x) == np.sign(h1.vx) and np.sign(y - h1.y) == np.sign(h1.vy) and np.sign(x - h2.x) == np.sign(h2.vx) and np.sign(y - h2.y) == np.sign(h2.vy)):
+                    collisions += 1
+    return collisions
+
+
 def find_collisions() -> int:
     hailstones = create_hailstones()
-    return count_collisions(hailstones, 0, 40)
+    return collisions_within_bounds(hailstones, 200000000000000, 400000000000000)
 
 print(find_collisions())
