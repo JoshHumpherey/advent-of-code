e23a90a5ac2c2e3b87c3b957580d9d58b69082ae
JoshHumpherey <jhumphrey321@gmail.com>
2024-11-28 11:58:58 -0600

    Add Matrix parsing for Golang

--- lib/parse.go
+++ lib/parse.go
@@ -3,10 +3,11 @@ package lib
 import (
 	"bufio"
 	"os"
+	"strconv"
 )
 
-func ParseStrings(file_path string) ([]string, error) {
-	file, err := os.Open(file_path)
+func ParseStrings(filePath string) ([]string, error) {
+	file, err := os.Open(filePath)
 	if err != nil {
 		return nil, err
 	}
@@ -19,4 +20,45 @@ func ParseStrings(file_path string) ([]string, error) {
 		lines = append(lines, scanner.Text())
 	}
 	return lines, nil
+}
+
+func ParseStringMatrix(filePath string) ([][]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	grid := make([][]string, 0)
+	for scanner.Scan() {
+		row := make([]string, 0)
+		for _, val := range scanner.Text() {
+			row = append(row, string(val))
+		}
+		grid = append(grid, row)
+	}
+	return grid, nil
+}
+
+func ParseIntegerMatrix(filePath string) ([][]int, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	grid := make([][]int, 0)
+	for scanner.Scan() {
+		row := make([]int, 0)
+		for _, val := range scanner.Text() {
+			i, _ := strconv.Atoi(string(val))
+			row = append(row, i)
+		}
+		grid = append(grid, row)
+	}
+	return grid, nil
 }
